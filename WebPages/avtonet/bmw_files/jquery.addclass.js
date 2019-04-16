d1e858fc6b37c56194db52a54ac0188b0bbe9931
STAVANJA <js8927@student.uni-lj.si>
2019-04-16 19:46:46 +0200

    Move data/WebPages to input folder

--- WebPages/avtonet/bmw_files/jquery.addclass.js
+++
@@ -1,7 +0,0 @@
-$(window).load(function()
-{
-	var height = document.getElementById("bannercek").offsetHeight;
-		if (height >50) {
-			document.getElementById('bannercek').className += ' BannerBoxSmart972';
-		}
-});
