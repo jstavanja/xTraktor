e51147a5cddbed1883bbdc69c4c2188750d74d43
STAVANJA <js8927@student.uni-lj.si>
2019-04-17 22:11:42 +0200

    RegExp implementation for overstock.com

--- implementation/regex.py
+++ implementation/regex.py
@@ -4,7 +4,7 @@ from typing import List
 
 
 class RegexParser:
-    def get_html_from_file(self, file_path: str) -> str:
+    def get_html_from_file(self, file_path: str, encoding=None) -> str:
         """
         Gets raw HTML from a saved .html file.
 
@@ -18,7 +18,10 @@ class RegexParser:
         str
             HTML content fetched from the file in a string.
         """
-        return open(file_path, 'r', encoding='utf-8').read()
+        if encoding is not None:
+            return open(file_path, 'r', encoding=encoding).read()
+        else:
+            return open(file_path, 'r').read()
 
     def parse_rtvslo(self, html: str) -> str:
         """
@@ -59,6 +62,56 @@ class RegexParser:
 
         return json.dumps(parsed_content)
 
+    def parse_overstock(self, html):
+        """
+        Parses all the necessary information from the HTML content provided and
+        return a JSON object in a string.
+
+        The information acquired from the HTML contains: Title, Content, Price, ListPrice, Saving, SavingPercent, Content
+
+        Parameters
+        ----------
+        html:
+            HTML content to parse information from.
+
+        Returns
+        -------
+        str:
+            JSON object with the information in the string format.
+        """
+        title_regex = r"<td><a href=\"http:\/\/www\.overstock\.com\/cgi-bin\/d2\.cgi\?PAGE=PROFRAME[\S\s]*?<b>(.*)<\/b>"
+        list_price_regex = r"<b>List Price:[\S\s]*?<s>(.*)<\/s>"
+        price_regex = r"<b>Price:[\S\s]*?<b>(.*)<\/b>"
+        saving_regex = r"<span class=\"littleorange\">(.*)\s\("
+        saving_percent_regex = r"<span class=\"littleorange\">[\S\s]*?\((.*)\)<\/span>"
+        content_regex = r"<span class=\"normal\">([\S\s]*?)<br"
+
+        # find list of lists, that include all matches for each regex
+        regex_matches = self.find_all_matches(html=html,
+                                              regex_list=[title_regex, list_price_regex, price_regex, saving_regex,
+                                                          saving_percent_regex, content_regex])
+
+        # bundle them into items
+        items = list(zip(*regex_matches))
+
+        # add labels to parsed elements (in the same order they were parsed by regexes into the regex_matches)
+        items_processed = []
+        for item in items:
+            items_processed.append({
+                "title": item[0],
+                "list_price": item[1],
+                "price": item[2],
+                "saving": item[3],
+                "saving_percent": item[4],
+                "content": item[5],
+            })
+
+        parsed_content = {
+            "items": items_processed
+        }
+
+        return json.dumps(parsed_content)
+
     def find_first_matches(self, html: str, regex_list: List[str]) -> List[str]:
         """
         Finds first occurences of regex matches and returns them, bundled as a list of strings.
@@ -81,11 +134,35 @@ class RegexParser:
             matches.append(compiled.group(1))
         return matches
 
+    def find_all_matches(self, html: str, regex_list: List[str]) -> List[List]:
+        """
+        Finds all occurences of regex matches and returns them, bundled as a list of lists of strings.
+
+        Parameters
+        ----------
+        html:
+            HTML source to search for occurences in.
+        regex_list:
+            List of regular expressions that will be compiled and evaluated on the input HTML.
+
+        Returns
+        -------
+        List[List]:
+            List of lists of regex matches as strings.
+        """
+        matches = []
+        for regex in regex_list:
+            compiled = re.findall(regex, html)
+            matches.append(compiled)
+        return matches
+
 
 if __name__ == '__main__':
     rp = RegexParser()
 
-    pageContent = rp.get_html_from_file(
-        '../input/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html')
+    # pageContent = rp.get_html_from_file(
+    #     '../input/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html', encoding="utf-8")
+    # print(rp.parse_rtvslo(pageContent))
 
-    print(rp.parse_rtvslo(pageContent))
+    pageContent = rp.get_html_from_file('../input/overstock.com/jewelry01.html')
+    print(rp.parse_overstock(pageContent))
