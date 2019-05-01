522daa1226f5e1091d86f58f12e9c1a9f373cb43
Matej Klemen <matej.klemen1337@gmail.com>
2019-05-01 10:14:20 +0200

    Add encoding parameter to docstring

--- implementation/utils.py
+++ implementation/utils.py
@@ -4,13 +4,18 @@ def get_html_from_file(file_path, encoding=None):
 
     Parameters
     ----------
-    file_path : string
-        Path to the file containing the HTML.
+    file_path: str
+        Path to the file containing the HTML
+
+    encoding: str, optional
+        Encoding to use for reading an HTML file. Common options are
+        'utf8', 'cp1250', 'iso 8859-1'. If not specified, UTF-8 will
+        be used
 
     Returns
     -------
     str
-        HTML content fetched from the file in a string.
+        HTML content fetched from the file in a string
     """
     if encoding is not None:
         return open(file_path, 'r', encoding=encoding).read()
